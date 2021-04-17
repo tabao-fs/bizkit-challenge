8cadb9ff70a955d8c1398dc84ba1ec4c4f39f76a
Tom <abaotom14@gmail.com>
2021-04-17 08:34:03 +0800

    implement getMinimumCost

--- Greedy Candy Store Owner/greedy_candy_store_owner.py
+++ Greedy Candy Store Owner/greedy_candy_store_owner.py
@@ -6,9 +6,34 @@ import random
 import re
 import sys
 
+
+def get_multiplier(k, length):
+    multiplier = []
+
+    if k == 1:
+        for i in range(1, length + 1):
+            multiplier.append(i)
+        return multiplier
+
+    for i in range(1, k + 1):
+        l = [i] * k
+        multiplier += l
+        if len(multiplier) >= length:
+            break
+    return multiplier
+
+
 # Complete the getMinimumCost function below.
 def getMinimumCost(k, c):
-    pass
+    min_cost = 0
+    length = len(c)
+    c.sort(reverse=True)
+    multiplier = get_multiplier(k, length)
+
+    for i in range(length):
+        min_cost += c[i] * multiplier[i]
+    
+    return min_cost
 
 
 if __name__ == "__main__":
